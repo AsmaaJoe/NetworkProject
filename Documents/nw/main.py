b67bb1abc22fce6a4a90446bb776be124c38e2f3
nada <nada.abdelbaky1996@gmail.com>
2017-12-23 13:45:57 +0200

    Start capturing

--- Documents/nw/main.py
+++ Documents/nw/main.py
@@ -3,7 +3,7 @@ import sys # We need sys so that we can pass argv to QApplication
 
 import project # This file holds our MainWindow and all design related things
               # it also keeps events etc that we defined in Qt Designer
-from scapy.all import*
+from scapy.all import *
 
 class ExampleApp(QtGui.QMainWindow, project.Ui_MainWindow):
     def __init__(self):
@@ -13,13 +13,38 @@ class ExampleApp(QtGui.QMainWindow, project.Ui_MainWindow):
         # access variables, methods etc in the design.py file
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
-        pkts=sniff(count=3)
-        self.textEdit.setText(str(pkts))
+        ##pkts=sniff(count=3,prn=self.textEdit.setText(lambda x: x.custom_action()))
+        #packet= sniff(count=3)
+        
+        # for i in range (5):
+        #     packet= sniff(count=1)
+        #     self.textEdit.append('Packet : {} ==> {}'.format( packet[0][1].src, packet[0][1].dst)   )
+        ##self.textEdit.setText(str(pkts[0]))
+        ##Qstring y = pkts[0]
+        self.pushButton.clicked.connect(self.b1_clicked)
+        self.pushButton_2.clicked.connect(self.b2_clicked)
+      ##  self.textEdit.setText(str(pkts[0]))
                             # It sets up layout and widgets that are defined
-		
-		
-		
+  
+    def b1_clicked(self):
+         for i in range (5):
+            packet= sniff(count=1)
+            self.textEdit.append('Packet : {} ==> {}'.format( packet[0][1].src, packet[0][1].dst)   )
+    def b2_clicked(self):
+        self.textEdit.setText('')
 
+     # for i in range(10):
+     #    print i
+     #    if self.pushButton:
+     #        break
+     #    time.sleep(0.5)    
+
+		
+		
+def custom_action(packet):
+    global counter
+    counter += 1
+    return   'Packet #{}: {} ==> {}'.format(counter, packet[0][1].src, packet[0][1].dst)   
 def main():
 	
     app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
